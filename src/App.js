f1626ca8179ca97b77c3ffa2d983195b76668f35
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-23 21:24:44 +0600

    implemented folder structure and react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
-  return <Navbar>hello world</Navbar>;
+  return (
+    <>
+      <ToastContainer />
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Register />}></Route>
+        </Routes>
+      </Navbar>
+    </>
+  );
 }
 
 export default App;
