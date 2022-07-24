d336c1e0777eab74b440a161c018669abd2206d6
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-25 00:02:09 +0600

    setup redux for updated tour

--- src/redux/api.js
+++ src/redux/api.js
@@ -18,4 +18,6 @@ export const createTour = (tourData) => API.post('/tour', tourData);
 export const getTours = () => API.get('/tour');
 export const getTour = (id) => API.get(`/tour/${id}`);
 export const deleteTour = (id) => API.delete(`/tour/${id}`);
+export const updateTour = (updatedTourData, id) =>
+  API.patch(`/tour/${id}`, updatedTourData);
 export const getToursByUser = (id) => API.get(`/tour/userTours/${id}`);
